a3d8fca520032574b0c76b43feb571acfb9ad470
WahyuAprian <193040141.wahyu@mail.unpas.ac.id>
2021-10-16 19:57:51 +0700

    Membuat dan menambahkan file sesuai dengan video ke-4 (Laravel bagian 2) pertemuan ke-6

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,6 +5,8 @@
   <h1 class="mb-5">{{ $post->title }}</h1>
   {{-- <h5>{{ $post["author"] }}</h5> --}}
   
+<p>By. Wahyu Aprian in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
   {!! $post->body !!}
 
 {{-- </article> --}}
